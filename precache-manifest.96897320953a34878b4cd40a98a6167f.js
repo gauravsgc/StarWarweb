247b530a69e684e1fe7ce23146b1f185281e24cd
Gaurav <pgauravpandeydit@gmail.com>
2020-09-12 21:12:50 +0530

    Updates

---
+++ precache-manifest.96897320953a34878b4cd40a98a6167f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc94d1097963d133aaa4a3855f15f1f",
+    "url": "/starwarweb/index.html"
+  },
+  {
+    "revision": "09c57f6737cce719df6b",
+    "url": "/starwarweb/static/css/main.2ee44be3.chunk.css"
+  },
+  {
+    "revision": "bfbcfecc97775949fc3d",
+    "url": "/starwarweb/static/js/2.8a86f67e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/starwarweb/static/js/2.8a86f67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c57f6737cce719df6b",
+    "url": "/starwarweb/static/js/main.a5a2374f.chunk.js"
+  },
+  {
+    "revision": "76b2d16fdde45ac7f2d0",
+    "url": "/starwarweb/static/js/runtime-main.4ce9af42.js"
+  }
+]);
